17e0fbe37d7be29fbbfb41ea650fb8252d244d4f
karthikeyan <113494526+Karthi7170@users.noreply.github.com>
2026-09-18 23:03:23 +0530

    Change portfolio auto-swipe interval to 3 seconds

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -34,7 +34,7 @@ export default function ProjectsPage() {
     const timer = window.setInterval(() => {
       advanceRail(youtubeRail.current)
       advanceRail(portfolioRail.current)
-    }, 5000)
+    }, 3000)
 
     return () => window.clearInterval(timer)
   }, [])
